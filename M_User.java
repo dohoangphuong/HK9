603a41efccb3b6618ef184b5f8e385038a89e8a7
Hoàng Phương <phuongdohoang93@gmail.com>
2017-04-26 09:49:40 +0700

    Create M_User.java

---
+++ M_User.java
@@ -0,0 +1,40 @@
+package net.viralpatel.struts2.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "M_User")
+public class M_User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String password;
+
+	public M_User() {
+
+	}
+
+	@Id
+	@Column(name = "username")
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Column(name = "password")
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
